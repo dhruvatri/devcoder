37528974e5bffa0f19f236421b25e3c6d06212e8
Aditya <aditya298k@gmail.com>
2024-11-13 22:22:20 +0530

    added ui folder

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+// File to export all the UI components
+// exp -> export {default as Button} from './Button';
+// exp -> export {default as Input} from './Input';
+// exp -> export {default as List} from './List';
+
+// {UI components} are components that are shared among multiple parent components, exp - Button, Input, Lists, etc.
