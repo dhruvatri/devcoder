e1914a9e79e0c663e703d2f28f0c67166b6dfa64
Aditya <i-aditya.mishra@devrev.ai>
2024-11-15 11:16:16 +0530

    merged from aditya/binit/dharineesh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignUpForm from "./components/SignUpForm/index.tsx";
 import Login from "./components/Login/Login";
-import SelectRolePage from "./components/SelectRolePage/SelectRolePage";
+import SelectRolePage from "./components/SelectRolePage";
 import { GlobalChat } from "./components";
 
 const App: React.FC = () => {
