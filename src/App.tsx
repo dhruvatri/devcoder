b188b42690896e4220f5d8b5142525f368ef26f0
Aditya <aditya298k@gmail.com>
2024-11-16 20:41:47 +0530

    testing problemcomponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Login from "./components/Login/Login";
 import { GlobalChat, UserDashBoard } from "./components";
 import SelectRolePage from "./components/SelectRolePage";
 import SignUpForm from "./components/SignUpForm";
+import CodeEditorComponent from "./components/CodeEditorComponent";
 
 const App: React.FC = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<UserDashBoard />} />
+				<Route path="/" element={<CodeEditorComponent />} />
 				<Route path="/chat" element={<GlobalChat />} />
 				<Route path="/signup" element={<SignUpForm />} />
 				<Route path="/login" element={<Login />} />
