1a98e8d3b4c0d9bd5e234b97609c22404b41c7d9
Dhara2003 <vz7116@srmist.edu.in>
2024-11-17 12:04:19 +0530

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
-
-import './index.css';
-import LandingPage from './LandingPage/LandingPage';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { UserCircle2, Code2, FileCode2, User, LogOut } from 'lucide-react';
+import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <div className="navbar-container">
+            <div className="navbar-logo">
+              <Code2 className="navbar-logo-icon" />
+              <span className="navbar-logo-text">Dev Editor</span>
+            </div>
+            <div className="navbar-menu">
+              <Link to="/problemset" className="navbar-item">
+                <FileCode2 className="navbar-icon" />
+                <span>Problems</span>
+              </Link>
+              <Link to="/profile" className="navbar-item">
+                <User className="navbar-icon" />
+                <span>Profile</span>
+              </Link>
+              <button className="navbar-item">
+                <LogOut className="navbar-icon" />
+                <span>Logout</span>
+              </button>
+              <div className="navbar-profile">
+                <UserCircle2 className="navbar-profile-icon" />
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" />
+          <Route path="/problemset" />
+          <Route path="/profile"  />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
 
 
 
